553b9f642c3228b149663228c5495da5d84d0143
xuan.nguyen <xuan.nguyen@seldatinc.com>
2019-10-03 15:29:36 +0700

    initi

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Http\Controllers\Auth;
+
+
+use App\Http\Controllers\BaseController;
+
+class AuthController extends  BaseController
+{
+    public function __construct()
+    {
+    }
+
+    public function login(){
+
+    }
+
+    public function logout(){
+
+    }
+}
